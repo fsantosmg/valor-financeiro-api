b763b85f03144b606d39dcee4d87559e54288ccb
Felipe de Souza Santos <fssantos@grupomult.com.br>
2019-02-18 10:38:55 -0300

    inicio

---
+++ src/main/java/net/valorweb/financeiro/ValorFinanceiroApiApplication.java
@@ -0,0 +1,13 @@
+package net.valorweb.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorFinanceiroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValorFinanceiroApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/valorweb/financeiro/ValorFinanceiroApiApplicationTests.java
@@ -0,0 +1,16 @@
+package net.valorweb.financeiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ValorFinanceiroApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
